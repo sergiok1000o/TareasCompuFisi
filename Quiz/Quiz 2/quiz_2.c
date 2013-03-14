7ca575301fc42e42dc4a68079c44379a427cd3e2
SERGIO CAMILO SILVA PENA <sergiok1000o@hotmail.com>
2013-03-14 14:09:20 -0500

    quiz 2

---
+++ Quiz/Quiz 2/quiz_2.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main (int argc, char **argv)
+{
+	double a = atof(argv[1]);
+	double b = atof(argv[2]);
+	double h = atof(argv[3]);
+	int i;
+	int tamano;
+	double k = a;
+	double integral = 0.0;
+	double fun, fun2;
+
+
+	tamano = (int)((b - a) / h);
+	
+
+	for(i=0;i<tamano;i++)
+	{
+		fun = 1/ (sqrt (1 + cos(k)*sin(k)) ) ;
+		k = a + h;
+		integral = integral + fun * h;
+	}
+ 	
+        printf(" el resultado de la integral es %f \n",integral);
+
+	return 0;
+}
+
+
